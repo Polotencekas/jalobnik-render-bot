3421ef795ab47f938a849f3070424027eebc7c64
Polotencekas <davidand123321@gmail.com>
2025-05-23 19:49:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import os
-import random
-import logging
-import sqlite3
-
-from fastapi import FastAPI
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-from aiogram.types import ParseMode, Message, ReplyKeyboardMarkup, KeyboardButton
-from aiogram.dispatcher.filters import CommandStart, Command
+from aiogram.types import ParseMode
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 API_TOKEN = os.getenv("BOT_TOKEN")
@@ -17,12 +11,12 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 @dp.message_handler(commands=["start"])
-async def cmd_start(message: Message):
+async def start_handler(message: types.Message):
     await message.answer("Бот запущен и готов к работе!")
 
 @dp.message_handler(commands=["help"])
-async def cmd_help(message: Message):
-    await message.answer("Вот список доступных команд: /start /help")
+async def help_handler(message: types.Message):
+    await message.answer("Список команд: /start, /help")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
