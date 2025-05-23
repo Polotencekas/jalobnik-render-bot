d7d6b3b85e8d474fe9b52c880c2be6e89a54282e
Polotencekas <davidand123321@gmail.com>
2025-05-23 20:30:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 import sqlite3
 
 from fastapi import FastAPI
-from aiogram import Bot, Dispatcher, types, start_polling
+from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
@@ -13,13 +13,17 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
 from aiogram import Router
+import asyncio
 
 API_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
 
-# Ваши обработчики и логика бота здесь
+# Твои хэндлеры и логика бота
 
-if __name__ == "__main__":
-    start_polling(dp)
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
